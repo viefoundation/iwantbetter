eb9457afa2d30444660ccc32aa64ac575f13ac61
Austen Payan <austenpayan@gmail.com>
2014-10-02 16:27:45 -0700

    Fix database credentials for production environment

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'iwantbetter',
-			'username'  => 'root',
-			'password'  => 'root',
+			'database'  => 'kinglyco_iwantbetter',
+			'username'  => 'kinglyco',
+			'password'  => '[Kingandwife1]',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
