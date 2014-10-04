2fe3ef3bb0131ea8638f27ab97189c68ded84b33
Austen Payan <austenpayan@gmail.com>
2014-10-03 17:23:29 -0700

    Fix favicon paths

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -17,13 +17,13 @@
         <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('apple-touch-icon-120x120.png') }}">
         <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('apple-touch-icon-76x76.png') }}">
         <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('apple-touch-icon-152x152.png') }}">
-        <link rel="icon" type="image/png" href="{{ asset('favicon-196x196.png')}}" sizes="196x196">
-        <link rel="icon" type="image/png" href="{{ asset('favicon-160x160.png' }}" sizes="160x160">
-        <link rel="icon" type="image/png" href="{{ asset('favicon-96x96.png' }}" sizes="96x96">
-        <link rel="icon" type="image/png" href="{{ asset('favicon-16x16.png' }}" sizes="16x16">
-        <link rel="icon" type="image/png" href="{{ asset('favicon-32x32.png' }}" sizes="32x32">
+        <link rel="icon" type="image/png" href="{{ asset('favicon-196x196.png') }}" sizes="196x196">
+        <link rel="icon" type="image/png" href="{{ asset('favicon-160x160.png') }}" sizes="160x160">
+        <link rel="icon" type="image/png" href="{{ asset('favicon-96x96.png') }}" sizes="96x96">
+        <link rel="icon" type="image/png" href="{{ asset('favicon-16x16.png') }}" sizes="16x16">
+        <link rel="icon" type="image/png" href="{{ asset('favicon-32x32.png') }}" sizes="32x32">
         <meta name="msapplication-TileColor" content="#da532c">
-        <meta name="msapplication-TileImage" content="asset('mstile-144x144.png' }}">
+        <meta name="msapplication-TileImage" content="{{ asset('mstile-144x144.png') }}">
         <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
         <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
