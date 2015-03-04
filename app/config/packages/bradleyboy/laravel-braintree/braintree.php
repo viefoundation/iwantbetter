b7f41caf698ca47c553592bd13c5b5e4452b7aad
Austen Payan <austenpayan@gmail.com>
2015-03-04 10:58:03 -0800

    refactor donation controller

--- app/config/packages/bradleyboy/laravel-braintree/braintree.php
+++ app/config/packages/bradleyboy/laravel-braintree/braintree.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-	'environment'             => 'production',
-	'merchantId'              => 'cfgbfs86gv3572gm',
-	'publicKey'               => 'yzsr6nbrss8tkf3d',
-	'privateKey'              => 'c37a6bf32c481939990709451271a107',
-	'clientSideEncryptionKey' => 'my-client-side-encryption-key',
+	'environment'             => $_ENV['braintree.environment'],
+	'merchantId'              => $_ENV['braintree.merchantId'],
+	'publicKey'               => $_ENV['braintree.publicKey'],
+	'privateKey'              => $_ENV['braintree.privateKey'],
+	'clientSideEncryptionKey' => $_ENV['braintree.clientSideEncryptionKey']
 );
--- app/controllers/DonationController.php
+++ app/controllers/DonationController.php
@@ -4,33 +4,33 @@ class DonationController extends BaseController {
 
 	public function create() {
 
-		$create_customer = Braintree_Customer::create([
+		try {
+			
+			$create_customer = Braintree_Customer::create([
 
-			"firstName"       => Input::get('first_name'),
-			"lastName"        => Input::get('last_name'),
-			'email'           => Input::get('email_address'),
-			"creditCard"      => [
-			    "number"          => Input::get('number'),
-			    "expirationMonth" => Input::get('month'),
-			    "expirationYear"  => Input::get('year'),
-			    "cvv"             => Input::get('cvv'),
-			    "billingAddress"  => [
-			        "postalCode"      => Input::get('postal_code'),
-			        "streetAddress"   => Input::get('mailing_address'),
-			        "locality"        => Input::get('city'),
-			        "region"          => Input::get('state')
-			    ]
-			]
+				"firstName"       => Input::get('first_name'),
+				"lastName"        => Input::get('last_name'),
+				'email'           => Input::get('email_address'),
+				"creditCard"      => [
+				    "number"          => Input::get('number'),
+				    "expirationMonth" => Input::get('month'),
+				    "expirationYear"  => Input::get('year'),
+				    "cvv"             => Input::get('cvv'),
+				    "billingAddress"  => [
+				        "postalCode"      => Input::get('postal_code'),
+				        "streetAddress"   => Input::get('mailing_address'),
+				        "locality"        => Input::get('city'),
+				        "region"          => Input::get('state')
+				    ]
+				]
 
-		]);
+			]);
 
-		$amount = Input::get('amount');
+			$amount = Input::get('amount');
 
-		if ($create_customer->success) {
-
-			return Redirect::route('subscribeCustomer', ['customerId' => $result->customer->id, 'amount' => $amount]);
-
-		} else {
+		} catch (Exception $e) {
+			
+			echo ($e)
 
 			echo("Validation errors:<br/>");
 
@@ -39,6 +39,13 @@ class DonationController extends BaseController {
 			}
 
 			echo("</br><a href='javascript:javascript:history.go(-1)'>Go Back</a>");
+
+		}
+		
+
+		if ($create_customer->success) {
+
+			return Redirect::route('subscribeCustomer', ['customerId' => $result->customer->id, 'amount' => $amount]);
 
 		}
 
